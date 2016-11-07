bb7867510ac06732d638f63b573999fd021789dc
theoneneoxu <theoneneoxu@gmail.com>
2016-11-07 16:26:27 -0500

    Neo is checking in.

--- app/src/main/java/com/example/deepa/ufsocial/Landing.java
+++ app/src/main/java/com/example/deepa/ufsocial/Landing.java
@@ -1,5 +1,8 @@
 package com.example.deepa.ufsocial;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,19 @@ public class Landing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        //hide action bar
+        getSupportActionBar().hide();
+
+        //stay at this activity for 1.5s and then show Sign In
+        Handler h = new Handler(){
+            @Override
+            public void handleMessage(Message msg){
+                Intent intentToSignIn = new Intent(Landing.this, SignIn.class);
+                startActivity(intentToSignIn);
+                finish();
+            }
+        };
+        h.sendEmptyMessageDelayed(0, 1500);
     }
 }
