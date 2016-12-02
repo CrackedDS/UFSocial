0325a50f3d3c96122078e91c8acd355b50a0b0b5
Deepan Sekar <deepan.sek@gmail.com>
2016-12-02 00:32:10 -0500

    editing my profile page

--- app/src/main/java/com/example/deepa/ufsocial/MyProfile.java
+++ app/src/main/java/com/example/deepa/ufsocial/MyProfile.java
@@ -4,9 +4,14 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MyProfile extends Fragment {
@@ -26,8 +31,23 @@ public class MyProfile extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_profile, container, false);
+        final Button buttonSignUp = (Button) view.findViewById(R.id.buttonMyProfile);
+        buttonSignUp.setOnClickListener(new View.OnClickListener(){
+            View parent = (View) buttonSignUp.getParent();
+            Spinner interests = (Spinner) parent.findViewById(R.id.spinner1);
+            EditText editCrossPathNumber = (EditText) parent.findViewById(R.id.editCrossPathNumber);
+            EditText editMinCommonInterest = (EditText) parent.findViewById(R.id.editMinCommonInterest);
+            //EditText editTextConfirmPassword = (EditText) findViewById(R.id.editTextSignUpConfirmPassword);
+            Spinner education = (Spinner) parent.findViewById(R.id.spinner2);
+            @Override
+            public void onClick(View view){
+//                Toast.makeText(MyProfile.this, "msg"+String.valueOf(interests.getSelectedItem()),Toast.LENGTH_LONG).show();
+                Log.d("hi from ", "myprofile");
+            }
+        });
 
+        return view;
     }
 
 }
